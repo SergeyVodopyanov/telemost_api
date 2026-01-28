a239f21274edd776056acd74edf534f5c437c14d
SergeyPC <vodopyanov03@bk.ru>
2026-01-28 22:57:43 +0300

    стандартные роуты аутентификации

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\Auth\LoginRequest;
+use App\Http\Requests\Auth\RegisterRequest;
+use App\Models\User;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+
+class AuthController extends Controller
+{
+    public function register(RegisterRequest $request): JsonResponse
+    {
+        /** @var array{name: string, email: string, password: string} $data */
+        $data = $request->validated();
+
+        $user = User::create([
+            'name' => $data['name'],
+            'email' => $data['email'],
+            'password' => Hash::make((string) $data['password'])
+        ]);
+
+        $token = $user->createToken('auth-token')->plainTextToken;
+
+        return response()->json([
+            'user' => $user,
+            'token' => $token,
+            'message' => 'Регистрация успешно пройдена'
+        ], 201);
+    }
+
+    public function login(LoginRequest $request): JsonResponse
+    {
+        /** @var array{name: string, email: string, password: string} $data */
+        $data = $request->validated();
+
+        if (!Auth::attempt($data)) {
+            return response()->json([
+                'message' => 'Неправильный логин или пароль'
+            ], 401);
+        }
+
+        $user = User::where('email', $request->email)->firstOrFail();
+
+        $token = $user->createToken('auth-token')->plainTextToken;
+
+        return response()->json([
+            'user' => $user,
+            'token' => $token,
+            'message' => 'Аутентификация успешно пройдена'
+        ]);
+    }
+
+    public function logout(Request $request): JsonResponse
+    {
+        $request->user()?->currentAccessToken()?->delete();
+
+        return response()->json([
+            'message' => 'Успешный выход из профиля'
+        ]);
+    }
+}
